b8838d71aa09c902271e3ffc486553b698800961
Viviana Baron <84995277+vivianaybl@users.noreply.github.com>
2023-07-01 20:22:16 -0500

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,69 @@
+function circulo() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Circulo");
+}
+
+/*function rectangulo() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Rectangulo");
+}
+
+function trangulo() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Triangulo");
+}
+function girar() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Girar");
+}
+function arriba() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Arriba");
+}
+
+function abajo() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Abajo");
+}
+function izquierda() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Izquierda");
+}
+function derecha() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Derecha");
+}
+function ocultar() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Ocultar");
+}
+/*function gif(){
+    let figura = document.getElementById("figura")
+
+    figura.classList.toggle("gif");
+}*/
+function degradado() {
+  let figura = document.getElementById("figura");
+
+  figura.classList.toggle("Degradado");
+}
+function panel() {
+  const panel = document.querySelector(".panelIzq");
+
+  panel.classList.toggle("activo");
+}
+
+function superior() {
+  const panel = document.querySelector(".panelsSP");
+
+  panel.classList.toggle("activo1");
+}
